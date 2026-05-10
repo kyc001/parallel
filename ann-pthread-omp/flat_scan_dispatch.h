791c7227415d67e8a277dcaee2f5bf16dcc36691
kyc001 <852117630@qq.com>
2026-05-10 19:34:49 +0800

    rebuild

--- ann-pthread-omp/flat_scan_dispatch.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#if defined(__AVX2__)
-  #include "flat_scan_avx2.h"
-#elif defined(__aarch64__) || defined(__ARM_NEON)
-  #include "flat_scan_simd.h"
-#else
-  #include "flat_scan.h"
-#endif
--- ann-pthread-omp/pq_scan_dispatch.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#if defined(__AVX2__)
-  #include "pq_scan_avx2.h"
-#elif defined(__aarch64__) || defined(__ARM_NEON)
-  #include "pq_scan_simd.h"
-#else
-  #error "pq_scan_dispatch.h requires AVX2 on x86 or NEON on AArch64."
-#endif
